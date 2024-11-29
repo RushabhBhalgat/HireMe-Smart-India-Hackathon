697275fe0e789de499e002b2e293f19545387c47
Rushabh <rushabhbhalgat123@gmail.com>
2024-11-29 12:02:44 +0530

    deleting

--- frontend/src/api/axiosInstance.js
+++
@@ -1,9 +0,0 @@
-// client/src/api/axiosInstance.js
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3600", // Base URL for your backend
-  withCredentials: true, // Allow cookies to be sent with requests for session-based auth
-});
-
-export default axiosInstance;
