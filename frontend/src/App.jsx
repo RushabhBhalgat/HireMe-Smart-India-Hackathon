883489756e5fe3b21946e5da45ae99016bc9f951
sameer <sameerbagul2004@gmail.com>
2024-12-03 20:35:38 +0530

    insights added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
+// App.js
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+// import AppNavBar from "./components/AppNavbar";
+import LandingNavbar from "./components/LandingNavbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
@@ -10,6 +14,7 @@ import ResetPassword from "./Pages/ResetPassword";
 import Profile from "./pages/Profile";
 import { useState } from "react";
 import TeamPage from "./pages/TeamPage";
+// --------
 import CommunityPage from "./Pages/CommunityAndPeerSupport/CommunityPage";
 
 import CreateInterview from "./Pages/InterviewSection/CreateInterview";
@@ -18,7 +23,6 @@ import Interview from "./Pages/InterviewSection/Interview";
 import InterviewDashboard from "./Pages/InterviewSection/InterviewDashboard";
 
 import Insightspage from "./Pages/IndustryInsightsPage/Insightspage";
-
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [name, setName] = useState("");
@@ -28,6 +32,15 @@ const App = () => {
     <div className="md:h-screen bg-purple-100">
       <BrowserRouter>
         <ToastContainer />
+
+        {/* <AppNavBar
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+        /> */}
         <div>
           <Routes>
             <Route
@@ -70,6 +83,7 @@ const App = () => {
               path="resetPassword"
               element={<ResetPassword isLoggedIn={isLoggedIn} />}
             />
+
             <Route
               path="profile"
               exact
@@ -83,13 +97,20 @@ const App = () => {
             <Route path="/create-interview" element={<CreateInterview />} />
             <Route path="/feedback" element={<Feedback />} />
             <Route path="/interview" element={<Interview />} />
-            <Route path="/interview-dashboard" element={<InterviewDashboard />} />
+            <Route
+              path="/interview-dashboard"
+              element={<InterviewDashboard />}
+            />
+            <Route
+              path="/interview-dashboard"
+              element={<InterviewDashboard />}
+            />
 
-            {/* Community Page */}
+            {/* Direct Routes for Interview Pages */}
             <Route path="/CommunityPage" element={<CommunityPage />} />
 
-            {/* Industry Insights Page */}
-            <Route path="/insights" element={<Insightspage />} />
+            {/* Direct Routes for Insights */}
+            <Route path="/Insightspage" element={<Insightspage />} />
           </Routes>
         </div>
       </BrowserRouter>
